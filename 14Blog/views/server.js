713532998f26e1a5206ac2e04d5e598ffb40633a
Nathan Farquhar-Kay <130664748+nathankay89@users.noreply.github.com>
2023-11-02 14:42:49 -0400

    file fix

--- 14Blog/views/server.js
+++ 14Blog/views/server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('../controllers');
-const helpers = require('./utils/helpers');
+const helpers = require('../utils/helpers');
 
-const sequelize = require('./config/connection');
+const sequelize = require('../config/connections');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
